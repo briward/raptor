583e297ce652ae0bb16676816700bd4a0ffe5ef6
Bri Ward <me@briward.com>
2024-10-27 19:11:25 +0000

    Add more router tests.

--- tests/router.test.ts
+++ tests/router.test.ts
@@ -1,15 +1,27 @@
-import { assertArrayIncludes, assertEquals } from "jsr:@std/assert";
+import {
+  assertEquals,
+  assertRejects,
+  assertArrayIncludes,
+} from "jsr:@std/assert";
 
-import { type Context, type Route, Router } from "../mod.ts";
+import {
+  Router,
+  Request,
+  Response,
+  type Route,
+  type Context as ContextType,
+} from "../mod.ts";
+
+import Context from "../src/http/context.ts";
 
 Deno.test("test router accepts new route", () => {
   const router = new Router();
 
   const route: Route = {
     name: "test.route",
-    pathname: new URLPattern("/test-route", "https://test.com"),
+    pathname: new URLPattern("/test-route", "http://test.com"),
     method: "GET",
-    action: (context: Context) => {
+    action: (context: ContextType) => {
       console.log(context);
     },
   };
@@ -26,16 +38,16 @@ Deno.test("test router accepts new routes", () => {
     name: "test.route_1",
     pathname: new URLPattern("/test-route-1", "https://test.com"),
     method: "GET",
-    action: (context: Context) => {
+    action: (context: ContextType) => {
       console.log(context);
     },
   };
 
   const routeTwo: Route = {
     name: "test.route_2",
-    pathname: new URLPattern("/test-route-2", "https://test.com"),
+    pathname: new URLPattern("/test-route-2", "http://test.com"),
     method: "GET",
-    action: (context: Context) => {
+    action: (context: ContextType) => {
       console.log(context);
     },
   };
@@ -46,4 +58,78 @@ Deno.test("test router accepts new routes", () => {
     routeOne,
     routeTwo,
   ]);
+});
+
+Deno.test("test route influences context response", async () => {
+  const router = new Router();
+
+  const route: Route = {
+    name: "test.route",
+    pathname: new URLPattern("/test-route", "http://test.com"),
+    method: "GET",
+    action: (context: ContextType) => {
+      context.response.body = JSON.stringify({
+        influence: true,
+      })
+    },
+  };
+
+  router.addRoute(route);
+
+  const context = new Context(
+    new Request(new URL('http://test.com/test-route')),
+    new Response(null)
+  );
+
+  await router.handle(context);
+
+  assertEquals(context.response.body as unknown, JSON.stringify({
+    influence: true,
+  }));
+});
+
+Deno.test("test unknown route throws not found", () => {
+  const router = new Router();
+
+  const route: Route = {
+    name: "test.route",
+    pathname: new URLPattern("/test-route", "http://test.com"),
+    method: "GET",
+    action: (context: ContextType) => {
+      context.response.body = JSON.stringify({
+        influence: true,
+      })
+    },
+  };
+
+  router.addRoute(route);
+
+  const context = new Context(
+    new Request(new URL('http://test.com/unknown-route')),
+    new Response(null)
+  );
+
+  assertRejects(() => router.handle(context));
+});
+
+Deno.test("test context contains route params", () => {
+  const router = new Router();
+
+  const route: Route = {
+    name: "test.route",
+    pathname: new URLPattern("/test/:id", "http://test.com"),
+    method: "GET",
+    action: () => {},
+  };
+
+  router.addRoute(route);
+
+  const context = new Context(
+    new Request(new URL('http://test.com/test/1')),
+    new Response(null)
+  );
+
+  router.handle(context).then(() => {
+    assertEquals(context.params.id, '1');
+  });
 });
