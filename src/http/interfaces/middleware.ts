e4b751114a557b08a4b280bb41e9cf45088a53e1
Bri Ward <me@briward.com>
2024-10-29 13:56:08 +0000

    Added container based middleware.

---
+++ src/http/interfaces/middleware.ts
@@ -0,0 +1,13 @@
+import type Context from "../context.ts";
+
+/**
+ * The middleware definition.
+ */
+export default interface Middleware {
+  /**
+   * The handler method for middleware.
+   *
+   * @param context The current HTTP context.
+   */
+  handler(context: Context): void;
+};
