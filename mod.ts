7b1e03e431ee13b7485fd109e59602543d8688b3
Bri Ward <me@briward.com>
2024-11-02 15:36:09 +0000

    Run linter and formatter.

--- mod.ts
+++ mod.ts
@@ -6,13 +6,22 @@ import Kernel from "./src/kernel.ts";
 import Request from "./src/http/request.ts";
 import Response from "./src/http/response.ts";
 import NotFound from "./src/error/not-found.ts";
-import Middleware from './src/http/middleware.ts';
+import Middleware from "./src/http/middleware.ts";
 import BadRequest from "./src/error/bad-request.ts";
 import ServerError from "./src/error/server-error.ts";
-import ServiceProvider from './src/container/service-provider.ts';
+import ServiceProvider from "./src/container/service-provider.ts";
 
 // Export all available interfaces/types.
 export type { Error } from "./src/error/interfaces/error.ts";
 export type { Context } from "./src/http/interfaces/context.ts";
 
-export { ServiceProvider, Middleware, BadRequest, Kernel, NotFound, Request, Response, ServerError };
+export {
+  BadRequest,
+  Kernel,
+  Middleware,
+  NotFound,
+  Request,
+  Response,
+  ServerError,
+  ServiceProvider,
+};
--- src/container/service-provider.ts
+++ src/container/service-provider.ts
@@ -1,13 +1,13 @@
 import { container, type DependencyContainer } from "npm:tsyringe@^4.8.0";
 
 export default abstract class ServiceProvider {
-  public container : DependencyContainer;
+  public container: DependencyContainer;
 
   constructor() {
     this.container = container;
   }
 
-  public register() : void {
+  public register(): void {
     //
   }
 }
