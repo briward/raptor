fec9666b2437b9d7a40cb0f2cd6145f29ce0da3b
Bri Ward <me@briward.com>
2025-04-25 09:50:19 +0100

    feat(errors): Update error handling to use dedicated kernel method 'catch' for DX.

---
+++ src/http/processors/response.ts
@@ -0,0 +1,28 @@
+// deno-lint-ignore-file no-explicit-any
+
+import type Context from "../context.ts";
+import type { Processor } from "../interfaces/processor.ts";
+
+/**
+ * The Response processor for HTTP responses.
+ */
+export default class ResponseProcessor implements Processor {
+  /**
+   * Handle the response and process if found.
+   *
+   * @param response Any HTTP response object.
+   * @param context The current HTTP context.
+   * @returns An HTTP response or null.
+   */
+  public process(response: any, _context: Context): Response | null {
+    if (response instanceof Response) {
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers,
+      });
+    }
+
+    return null;
+  }
+}
